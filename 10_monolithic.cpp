aca2d7eeb034862b1097bcb28d4f1ea8f41857ab
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-18 10:55:39 +0530

    Modified File

--- 10_monolithic.cpp
+++ 10_monolithic.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-int main(){
-    int length = 0,breadth = 0;
+int main()
+{
+    int length = 0, breadth = 0;
     printf("Enter the length and breadth of the rectangle: ");
-    cin>>length>>breadth;
+    cin >> length >> breadth;
 
     int area = length * breadth;
     int perimeter = 2 * (length + breadth);
 
-    printf("Area of the rectangle is %d\nPerimeter of the rectangle is %d\n",area,perimeter);
+    printf("Area of the rectangle is %d\nPerimeter of the rectangle is %d\n", area, perimeter);
 
     return 0;
 }
