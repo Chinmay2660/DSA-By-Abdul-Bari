16b832c9f6c613ce0fbce2b93ec4fe44a8ac8185
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-21 20:53:09 +0530

    Added Files

---
+++ Section 5 - Recursion/50_lets_code_recursion.c
@@ -0,0 +1,33 @@
+// //Tail
+// #include<stdio.h>
+
+// void fun(int n)
+// {
+//     if(n>0){
+//     printf("%d ",n);
+//     fun(n-1);
+//     }
+// }
+
+// int main(){
+//     int x = 3;
+//     fun(x);
+//     return 0;
+// }
+
+//Head
+#include<stdio.h>
+
+void fun(int n)
+{
+    if(n>0){
+    fun(n-1);
+    printf("%d ",n);
+    }
+}
+
+int main(){
+    int x = 3;
+    fun(x);
+    return 0;
+}
