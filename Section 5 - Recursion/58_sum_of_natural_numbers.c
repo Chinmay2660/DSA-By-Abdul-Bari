8072609442aa4233ef8031b67e488095bf5ac83b
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-23 18:36:29 +0530

    Added Files

---
+++ Section 5 - Recursion/58_sum_of_natural_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+//Recursive function to calculate sum of natural numbers
+int sum(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum(n - 1) + n;
+    }
+}
+//Iterative solution
+int Isum(int n)
+{
+    int s = 0;
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+        s += i;
+    }
+    return  s;
+}
+
+int main()
+{
+
+    int r;
+    r = Isum(5);
+    printf("%d\n", r);
+
+    return 0;
+}
