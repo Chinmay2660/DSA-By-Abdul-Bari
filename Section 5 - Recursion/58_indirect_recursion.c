01d3ef849601b6552ff4d9b9a5d109101c787933
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-23 17:38:29 +0530

    Added File

---
+++ Section 5 - Recursion/58_indirect_recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void funB(int n);
+void funA(int n)
+{
+    if (n > 0)
+    {
+        printf("%d ", n);
+        funB(n - 1);
+    }
+}
+
+void funB(int n)
+{
+    if (n > 1)
+    {
+
+        printf("%d ", n);
+        funA(n/2);
+    }
+}
+
+int main()
+{
+    funA(20);
+    return 0;
+}
