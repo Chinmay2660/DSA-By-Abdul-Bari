3969e1c3a035045cfa01ca08f551ed5f3545a7ec
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-23 20:49:23 +0530

    Added Files

---
+++ Section 5 - Recursion/66_power_recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+//9 Multiplication 
+int power(int m, int n)
+{
+    if (n == 0)
+        return 1;
+    return power(m, n - 1) * m;
+}
+
+//6 Multiplication
+int power1(int m, int n)
+{
+    if (n == 0)
+        return 1;
+    if (n % 2 == 0)
+        return power1(m * m, n / 2);
+    return m * power1(m * m, (n - 1) / 2);
+}
+int main()
+{
+    int r = power1(9, 3);
+    printf("%d ", r);
+    return 0;
+}
