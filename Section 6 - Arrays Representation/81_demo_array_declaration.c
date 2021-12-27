e70f65b0c4abece5ae2186886958442d94a3da4a
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-27 14:26:10 +0530

    Added Section 6 File

---
+++ Section 6 - Arrays Representation/81_demo_array_declaration.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int A[5];
+
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        printf("%u\n", &A[i]);
+    }
+    // int B[5] = {1, 2, 3, 4, 5};
+    // int C[10] = {2, 4, 6};
+    // int D[5] = 0;
+    // int E[] = {1, 2, 3, 4, 5};
+
+    // return 0;
+}
