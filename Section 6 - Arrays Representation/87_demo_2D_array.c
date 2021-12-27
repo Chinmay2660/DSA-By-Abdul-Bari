a1826857cfdc6b875e8336022aa3657ed6526878
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-27 16:40:39 +0530

    Added new file

---
+++ Section 6 - Arrays Representation/87_demo_2D_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int A[3][4] = {{1, 2, 3, 4}, {2, 4, 6, 8}, {1, 3, 5, 7}};
+
+    int *B[3];
+    int **C;
+    int i, j;
+
+    B[0] = (int *)malloc(4 * sizeof(int));
+    B[1] = (int *)malloc(4 * sizeof(int));
+    B[2] = (int *)malloc(4 * sizeof(int));
+
+    C = (int **)malloc(3 * sizeof(int *));
+    C[0] = (int *)malloc(4 * sizeof(int));
+    C[1] = (int *)malloc(4 * sizeof(int));
+    C[2] = (int *)malloc(4 * sizeof(int));
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+            printf("%d ", C[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
