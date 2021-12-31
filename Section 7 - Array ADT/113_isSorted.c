cddf759d817419248adc68a2f9f6ead2d99064d1
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2021-12-31 20:08:54 +0530

    Added new files

--- Section 7 - Array ADT/113_isSorted.c
+++ Section 7 - Array ADT/113_isSorted.c
@@ -13,6 +13,27 @@ void Display(struct Array arr)
     for (i = 0; i < arr.length; i++)
         printf("%d ", arr.A[i]);
 }
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void InsertSort(struct Array *arr, int x)
+{
+    int i = arr->length - 1;
+    if (arr->length == arr->size)
+        return;
+    while (i >= 0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
 int isSorted(struct Array arr)
 {
     int i;
@@ -23,9 +44,29 @@ int isSorted(struct Array arr)
     }
     return 1;
 }
+
+void Rearrange(struct Array *arr)
+{
+    int i, j;
+    i = 0;
+    j = arr->length - 1;
+    while (i < j)
+    {
+        while (arr->A[i] < 0)
+            i++;
+        while (arr->A[j] >= 0)
+            j--;
+        if (i < j)
+        {
+            swap(&arr->A[i], &arr->A[j]);
+        }
+    }
+}
 int main()
 {
-    struct Array arr1 = {{2, 3, 9, 16, 18, 21, 28, 32, 35}, 10, 9};
+    struct Array arr1 = {{2, 3, 9, -16, 18, 21, 28, 32, -35}, 10, 9};
+    Rearrange(&arr1);
+    InsertSort(&arr1, 20);
     printf("%d", isSorted(arr1));
     Display(arr1);
     return 0;
