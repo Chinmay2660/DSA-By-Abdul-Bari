42f0e60bd6b5e11428c23fa296865d2f3b09f754
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2022-01-08 02:01:28 +0530

    Added PDF

---
+++ Section 8 - Strings/ArrayInputCPP.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
