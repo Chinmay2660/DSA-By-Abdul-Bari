f2be40f4bbea02cab68ac02e745f37ae78dee908
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2022-01-08 21:48:29 +0530

    Add new file

--- Section 9 - Matrices/150_lower_triangular_matrix.cpp
+++ Section 9 - Matrices/150_lower_triangular_matrix.cpp
@@ -8,26 +8,37 @@ private:
     int *A;
 
 public:
+    LTMatrix()
+    {
+        n = 2;
+        ;
+        A = new int[2 * (2 + 1) / 2];
+    }
     LTMatrix(int n)
     {
         this->n = n;
         A = new int[n * (n + 1) / 2];
     }
-    ~LTMatrix() { delete[] A; }
-    void Display(bool row = true);
+    ~LTMatrix()
+    {
+        delete[] A;
+    }
+    void Display();
     void setRowMajor(int i, int j, int x);
     void setColMajor(int i, int j, int x);
     int getRowMajor(int i, int j);
     int getColMajor(int i, int j);
-    int getN() { return n; }
+    int getDimension()
+    {
+        return n;
+    }
 };
 
 void LTMatrix::setRowMajor(int i, int j, int x)
 {
     if (i >= j)
     {
-        int index = ((i * (i - 1)) / 2) + j - 1;
-        A[index] = x;
+        A[(i * (i - 1)) / 2 + j - 1] = x; // Row Major
     }
 }
 
@@ -35,8 +46,7 @@ void LTMatrix::setColMajor(int i, int j, int x)
 {
     if (i >= j)
     {
-        int index = (n * (j - 1) - (((j - 2) * (j - 1)) / 2)) + (i - j);
-        A[index] = x;
+        A[n * (j - 1) - (((j - 2) * (j - 1)) / 2) + (i - j)] = x; // Column major
     }
 }
 
@@ -44,8 +54,7 @@ int LTMatrix::getRowMajor(int i, int j)
 {
     if (i >= j)
     {
-        int index = ((i * (i - 1)) / 2) + j - 1;
-        return A[index];
+        return A[(i * (i - 1)) / 2 + j - 1]; // Row Major
     }
     else
     {
@@ -57,8 +66,7 @@ int LTMatrix::getColMajor(int i, int j)
 {
     if (i >= j)
     {
-        int index = (n * (j - 1) - (((j - 2) * (j - 1)) / 2)) + (i - j);
-        return A[index];
+        return A[n * (j - 1) - (((j - 2) * (j - 1)) / 2) + (i - j)]; // Column major
     }
     else
     {
@@ -66,7 +74,7 @@ int LTMatrix::getColMajor(int i, int j)
     }
 }
 
-void LTMatrix::Display(bool row)
+void LTMatrix::Display()
 {
     for (int i = 1; i <= n; i++)
     {
@@ -74,18 +82,12 @@ void LTMatrix::Display(bool row)
         {
             if (i >= j)
             {
-                if (row)
-                {
-                    cout << getRowMajor(i, j) << " ";
-                }
-                else
-                {
-                    cout << getColMajor(i, j) << " ";
-                }
+                // cout << A[n * (j - 1) - (((j - 2) * (j - 1)) / 2) + (i - j)] << " ";  //Column Major
+                cout << A[(i * (i - 1)) / 2 + j - 1] << " "; // Row Major
             }
             else
             {
-                cout << 0 << " ";
+                cout << "0 ";
             }
         }
         cout << endl;
@@ -94,35 +96,21 @@ void LTMatrix::Display(bool row)
 
 int main()
 {
-
-    LTMatrix rm(4);
-    rm.setRowMajor(1, 1, 1);
-    rm.setRowMajor(2, 1, 2);
-    rm.setRowMajor(2, 2, 3);
-    rm.setRowMajor(3, 1, 4);
-    rm.setRowMajor(3, 2, 5);
-    rm.setRowMajor(3, 3, 6);
-    rm.setRowMajor(4, 1, 7);
-    rm.setRowMajor(4, 2, 8);
-    rm.setRowMajor(4, 3, 9);
-    rm.setRowMajor(4, 4, 10);
-
-    rm.Display();
-    cout << endl;
-
-    LTMatrix cm(4);
-    cm.setColMajor(1, 1, 1);
-    cm.setColMajor(2, 1, 2);
-    cm.setColMajor(2, 2, 3);
-    cm.setColMajor(3, 1, 4);
-    cm.setColMajor(3, 2, 5);
-    cm.setColMajor(3, 3, 6);
-    cm.setColMajor(4, 1, 7);
-    cm.setColMajor(4, 2, 8);
-    cm.setColMajor(4, 3, 9);
-    cm.setColMajor(4, 4, 10);
-
-    cm.Display(false);
+    int d;
+    cout << "Enter Dimensions of Matrix: ";
+    cin >> d;
+    LTMatrix lt(d);
+    int x;
+    cout << "Enter All elements of Matrix: ";
+    for (int i = 1; i <= d; i++)
+    {
+        for (int j = 1; j <= d; j++)
+        {
+            cin >> x;
+            lt.setRowMajor(i, j, x);
+        }
+    }
+    lt.Display();
 
     return 0;
 }
